b514918585cdfa5ad519b75bb00b16c8c2078da3
prosbloom225 <prosbloom@gmail.com>
2015-03-25 16:11:30 -0400

    fixing log permissions.  moving workers to one log

--- reset.sh
+++ reset.sh
@@ -3,3 +3,5 @@ redis-cli -p 6380 flushall
 
 ./stopWorkers.sh
 ./stopMasters.sh
+
+rm -rf /var/log/crawler/
--- startWorkers.sh
+++ startWorkers.sh
@@ -1,6 +1,6 @@
 for i in `seq 1 $1`; 
 do
 	echo 'starting worker '$i	
-	nohup ./worker &> /var/log/crawler/worker$i.out
+	nohup ./worker &> /var/log/crawler/worker.out
 	sleep  .1
 done	
