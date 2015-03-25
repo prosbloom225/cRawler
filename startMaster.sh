7df64ff6db5735ae00f114d4d1add347af0e8f76
prosbloom225 <prosbloom@gmail.com>
2015-03-25 16:05:02 -0400

    moving logs to /var/log

--- startMaster.sh
+++ startMaster.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 	echo 'starting master 1'	
-	nohup  ./master 0 40000 &> master1.out&
+	nohup  ./master 0 40000 &> /var/log/crawler/master1.out&
 	sleep  .1
 	echo 'starting master 2'	
-	nohup  ./master 40000 80000 &> master2.out&
+	nohup  ./master 40000 80000 &> /var/log/crawler/master2.out&
 	sleep  .1
 	echo 'starting master 3'	
-	nohup  ./master 80000 120000 &> master3.out&
+	nohup  ./master 80000 120000 &> /var/log/crawler/master3.out&
 	sleep  .1
 	echo 'starting master 4'	
-	nohup  ./master 120000 160000 &> master4.out&
+	nohup  ./master 120000 160000 &> /var/log/crawler/master4.out&
 	sleep  .1
 	echo 'starting master 5'	
-	nohup  ./master 160000 200000 &> master4.out&
+	nohup  ./master 160000 200000 &> /var/log/crawler/master5.out&
 
--- startWorkers.sh
+++ startWorkers.sh
@@ -1,6 +1,6 @@
 for i in `seq 1 $1`; 
 do
 	echo 'starting worker '$i	
-	nohup ./worker &
+	nohup ./worker &> /var/log/crawler/worker$i.out
 	sleep  .1
 done	
