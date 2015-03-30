358b1193da27fdeb1cd4fc3e3a59c8ca4124ae81
prosbloom225 <prosbloom@gmail.com>
2015-03-30 16:08:52 -0400

    ok, logging should be working now

--- startWorkers.sh
+++ startWorkers.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 for i in `seq 1 $1`; 
 do
 	echo 'starting worker '$i	
-	nohup ./worker& >> /var/log/crawler/worker.out 
+	nohup ./worker &>> /var/log/crawler/worker.out&
 	sleep  .1
 done	
